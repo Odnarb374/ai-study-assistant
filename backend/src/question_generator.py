ac3aa50e4649a4f1df67562d9f91f210ef54d567
Odnarb374 <bsore1@umbc.edu>
2026-05-12 16:00:24 -0400

    Implemented question generator, first index is a question, second is the answer

--- backend/src/question_generator.py
+++ backend/src/question_generator.py
@@ -1,3 +1,65 @@
+from transformers import (
+    AutoTokenizer,
+    AutoModelForSeq2SeqLM
+)
+
+tokenizer = AutoTokenizer.from_pretrained(
+    "google/flan-t5-base"
+)
+
+model = AutoModelForSeq2SeqLM.from_pretrained(
+    "google/flan-t5-base"
+)
+
+
 def generate_questions(chunks):
-    questions = [["question", "answer"]]
+    questions = []
+
+
+    for chunk in chunks:
+        input_text = f"""
+Read the passage and create one factual quiz question.
+
+Passage:
+{chunk}
+
+Quiz Question: 
+"""
+        input_ids = tokenizer(input_text, return_tensors="pt").input_ids
+
+        outputs = model.generate(
+            input_ids,
+            max_new_tokens=100,
+            num_beams=5,
+            early_stopping=True
+        )
+        result = tokenizer.decode(outputs[0], skip_special_tokens=True)
+
+        print(result)
+        questions.append(result)
+
+        input_text = f"""
+Read the passage and answer the quiz question.
+
+Passage:
+{chunk}
+
+Quiz Question: 
+{result}
+
+Answer:
+"""
+        input_ids = tokenizer(input_text, return_tensors="pt").input_ids
+
+        outputs = model.generate(
+            input_ids,
+            max_new_tokens=100,
+            num_beams=5,
+            early_stopping=True
+        )
+        result = tokenizer.decode(outputs[0], skip_special_tokens=True)
+
+        print(result)
+        questions.append(result)
+
     return questions
