22098bcc111be102b0668a76a7e2da2d0f49ceab
Odnarb374 <bsore1@umbc.edu>
2026-05-12 17:54:54 -0400

    Implemented flashcards, added sentences to pipeline

--- backend/src/flashcards.py
+++ backend/src/flashcards.py
@@ -1,3 +1,40 @@
-def generate_flashcards(keywords):
-    flashcards = [["keyword", "definition"]]
+from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
+
+tokenizer = AutoTokenizer.from_pretrained(
+    "google/flan-t5-base"
+)
+
+model = AutoModelForSeq2SeqLM.from_pretrained(
+    "google/flan-t5-base"
+)
+
+def generate_flashcards(keywords,sentences):
+    flashcards = []
+
+
+    for keyword in keywords:
+        # find the keyword, and get surrounding sentences
+        context = ""
+        for i, s in enumerate(sentences):
+            if keyword.lower() in s.lower():
+                context = " ".join(sentences[max(0,i-1):i+2])
+                break
+        input_text = f"""
+Create a definition for this keyword based on the context
+
+Keyword: {keyword}
+Context: {context}
+
+Definition: 
+"""
+        input_ids = tokenizer(input_text, return_tensors="pt").input_ids
+
+        outputs = model.generate(
+            input_ids,
+            max_new_tokens=100,
+            num_beams=5,
+            early_stopping=True
+        )
+        result = tokenizer.decode(outputs[0], skip_special_tokens=True)
+        flashcards.append([keyword,result])
     return flashcards
